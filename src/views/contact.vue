28c2dfa8abfdf82550b6971ecd5dc74dfecd2446
Andrej Korica <koricaandrej3@gmail.com>
2022-06-07 13:36:16 +0200

    za pull

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -4,7 +4,7 @@
         <v-row justify="center" align="center">
             <v-col justify="center" align="center">
                 <v-card width="1250" class="px-15 py-10 mb-15" color="grey lighten-3">
-                  <v-text-field label="Name" name="firstName" color="#153040" class="mx-15"></v-text-field>
+                  <v-text-field label="Name" name="firstName" color="#153040" class="mx-15" ></v-text-field>
                   <v-text-field label="Email" name="email" color="#153040" class="mx-15"></v-text-field>
                   <v-textarea outlined name="body" label="Write a message" color="#153040" height="200" class="mx-15 my-8"></v-textarea>
                   <v-btn class="text-capitalize white--text" color="orange" width="250">send</v-btn>
@@ -16,7 +16,12 @@
 
 <script>
 export default {
+name: "contact",
+data(){
+return{
 
+}
+}
 }
 </script>
 
